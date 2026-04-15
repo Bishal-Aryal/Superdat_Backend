0356525162f3a4fbc5ea41b364da5c187365aa64
Bishal Aryal <bishal1225@gmail.com>
2026-04-15 17:12:41 +0545

    media removedd

--- product/models.py
+++ product/models.py
@@ -27,7 +27,7 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     average_rating = models.FloatField(null=True, blank=True)
-    image = models.ImageField(upload_to="media/product/image", null=True, blank=True)
+    image = models.ImageField(upload_to="product/image", null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
